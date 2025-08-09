b6f61fcfaadd18f56b7e0574fa98d47b64399474
Noah <n.schouame@gmail.com>
2025-08-09 11:12:51 +0200

    avancée

--- components/MDRender.tsx
+++ components/MDRender.tsx
@@ -6,6 +6,8 @@ import mdElementParsing from "../utils/mdElementParsing";
 import textBlockLineParsing from "../utils/textBlockLineParsing";
 import inlineParsing from "../utils/inlineParsing";
 
+import config from '../config.json';
+
 type MDRenderProps = {
     markdown: string;
 };
@@ -19,14 +21,28 @@ export default function MDRender({ markdown }: MDRenderProps) {
         if (block.type === 'code') {
             // Here : code block case -> 
             elements.push(
-                <div className="flex flex-col items-start items-center bg-neutral-950 ">
-                    <div>
-                        {block.language}
+                <div className={`flex flex-col items-start justify-start ${config.code.background.color} ${config.code.rounded.size} max-w-full p-2 gap-2 border border-neutral-800 ${config.code.width}`}>
+                    <div className="flex flex-row items-center justify-between w-full">
+                        {config.code.language.enable && (
+                            <div className={`${config.code.language.color} ${config.code.language.size}`}>
+                                {block.language}
+                            </div>
+                        )}
+
+                        {config.code.copyIcon.enable && (
+                            <div className={`${config.code.language.color} ${config.code.language.size} cursor-pointer`}
+                                onClick={() => {}}
+                            >
+                                yes
+                            </div>
+                        )}
                     </div>
                     {Object.entries(block)
                         .filter(([key]) => !isNaN(Number(key))) // garde uniquement les clés numériques
                         .map(([_, line], index) => (
-                            <div key={index}>
+                            <div key={index}
+                                className=""
+                            >
                                 {line}
                             </div>
                     ))}
